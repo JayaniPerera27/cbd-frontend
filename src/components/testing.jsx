c9a89d7c37322e8cfb70b0903696d1c8e2fa6f95
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-26 18:12:52 +0530

    create incrementing button

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,12 +1,24 @@
 import "./testing.css"
 
 export default function Testing (){
+
+    let count = 0
+
+    function increment(){
+        console.log("incrementing")
+        count = count +1
+        console.log(count)
+
+    }
+   
     return(
         <div className="background">
             <button>-</button>
-            <span>0</span>
-            <button>+</button>
-            
+
+            <span>{count}</span>
+
+            <button onClick={increment}>+</button>
+
             
         </div>
     )
