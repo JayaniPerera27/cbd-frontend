fe7e2fe0c1a8b7709bfd2ef5a48b4d325e4345fd
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-26 18:06:00 +0530

    creating button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 //import { useState } from 'react'
 import './App.css'
-import ProductCard from './components/productCard'
-//import UserData from './components/userData'
+//import ProductCard from './components/productCard'
+import Testing from './components/testing'
+
+
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -9,10 +11,7 @@ function App() {
   return (
     <>
 
-    <ProductCard name="Laptop" src="https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg" price="$99.99"></ProductCard>
-    <ProductCard name="Tphone" price="$120.22"></ProductCard>
-    
-
+    <Testing/>
       
     </>
   )
