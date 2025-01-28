3f81ca9227d8e6f214d398335d1ffc0e96d5ca50
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-28 21:09:04 +0530

    Add Tailwind CSS to project

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage(){
+
     return (
         <div>
             <h1>Login Page</h1>
+            <input type="text" placeholder="Enter your user name"/>
+            <input type="password" placeholder="Enter your password" />
+            <button className="bg-yellow-500">Login</button> 
+            <Link to="/">Home</Link>
         </div>
     )
 }
