1a0ef0477d4a69ceffef582e64bfb7a70461a1d2
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-31 22:26:20 +0530

    Edit  img

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -1,7 +1,7 @@
 export default function UserData(){
     return(
         <>
-        <img src = "https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg"/>
+        <img src = "https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg" className="w-[200px] h-[200px] rounded-full"/>
         <span>Jayani Perera</span>
         <span>Online</span> 
         </>
