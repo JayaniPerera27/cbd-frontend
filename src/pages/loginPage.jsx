753ecd3cc5994d56740d9d7c6afbc51d32894f47
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-31 10:27:14 +0530

    Difference between margin and Padding and add a border

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -7,8 +7,10 @@ export default function LoginPage(){
             <h1>Login Page</h1>
             <input type="text" placeholder="Enter your user name"/>
             <input type="password" placeholder="Enter your password" />
-            <button className="bg-yellow-500">Login</button> 
+            <button className="bg-yellow-500 m-8 border-2 border-black p-2">Login</button>
+            <button className="bg-yellow-500 p-8 border-4 border-black">Login</button>
+
             <Link to="/">Home</Link>
         </div>
     )
-}
+}  
